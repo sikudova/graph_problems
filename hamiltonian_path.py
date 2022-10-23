3c0122ddcb61dd80f0b217b424621b5d68f16ee2
Lucie Šikudová <xsikudov@fi.muni.cz>
2022-10-23 13:42:35 +0200

    feat: hamiltonian path

--- hamiltonian_path.py
+++ hamiltonian_path.py
@@ -4,38 +4,66 @@ from graph import Graph
 # TODO: color edges if contained in the path
 # TODO: add to class as public function?
 
-def hamiltonian_path(graph: "Graph", node: int, path: list[int]):  # -> bool:
+
+def hamiltonian_path2(graph: "Graph", node: int, visited: list[bool], path: list[int]):
+    if len(path) == graph.size:
+        print("Hamiltonian path found: " + str(path))
+        return
+
+    print("neighbours of {}: {}".format(node, graph.get_neighbours(node)))
+    for each in graph.get_neighbours(node):
+        if not visited[each]:
+            visited[each] = True
+            path.append(each)
+            hamiltonian_path2(graph, each, visited, path)
+
+            visited[each] = False
+            path.pop()
+
+
+def hamiltonian_path(graph: "Graph", node: int, path: list[int], all_paths: list[list[int]]):  # -> bool:
     print("hamiltonian_path called on node={}, current path={}".format(node, path))
     if node not in path:
         path.append(node)
 
         if len(path) == graph.size:
-            print("Hamiltonian path found: " + str(path))
-            return True
+            print("hamiltonian path found: " + str(path))
+            all_paths.append(path)
+            return path
+            # print("Hamiltonian path found: " + str(path))
+            # return True
 
         print("neighbours: " + str(graph.get_neighbours(node)))
         for each in graph.get_neighbours(node):
-            if not hamiltonian_path(graph, each, path):
-                print("not found with node {}".format(each))
+            candidate = hamiltonian_path(graph, each, path, all_paths)
+            if candidate is not None:
+                return candidate
 
+            # if not hamiltonian_path(graph, each, path):
+            #     print("not found with node {}".format(each))
+        print("path {} is a dead end".format(path))
         path.pop()
+
     else:
         print("the node {} is already in the path, the condition for the hamiltonian path can not be fulfilled".format(
             node))
-        return False
+        # return False
 
 
 graph = Graph(7)
 graph.add_edge(0, 1)
 graph.add_edge(1, 2)
 graph.add_edge(1, 3)
+graph.add_edge(2, 4)
 graph.add_edge(2, 5)
 graph.add_edge(2, 6)
 graph.add_edge(3, 2)
 graph.add_edge(3, 4)
 graph.add_edge(4, 5)
 graph.add_edge(5, 2)
+graph.add_edge(5, 6)
 graph.add_edge(6, 0)
 graph.visualize()
-hamiltonian_path(graph, 0, [])
-# print(graph.get_neighbours(6))
+visited = [False for _ in range(graph.size)]
+visited[0] = True
+hamiltonian_path2(graph, 0, visited, [0])
