5526405b62749c1df9a42e8df4cc09188a2b8bc2
Lucie Šikudová <xsikudov@fi.muni.cz>
2023-02-24 19:02:57 +0100

    feat: plot show - node labels

--- graph_class.py
+++ graph_class.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import scipy as sp
 import os
+from queue import Queue
 from graphviz import Digraph
 
 
@@ -42,28 +43,39 @@ class Graph:
 
     @property
     def size(self) -> int:
-        return len(self.adj_matrix().todense())
+        return len(self.adj_matrix())
 
     def adj_matrix(self):
-        return nx.adjacency_matrix(self.G)
+        return (nx.adjacency_matrix(self.G)).todense()
 
     def print_adj_matrix(self) -> None:
-        print(self.adj_matrix().todense())
+        print(self.adj_matrix())
+
+    """
+        neighbours
+    """
+
+    def get_neighbours(self, node: int) -> list[int]:
+        neighbours = []
+        for i in range(self.size):
+            if self.adj_matrix()[node, i]:
+                neighbours.append(i)
+        return neighbours
 
     """
         adding edges and nodes
     """
 
-    def add_node(self, index: int, attr=None):
+    def add_node(self, node: Node, attr=None):
         if attr is None:
             attr = {}
-        self.__G.add_node(index, **attr)
-        if ("\t" + str(index) + " [label=" + str(index) + "]\n") not in self.__G_graphviz.body:
-            self.__G_graphviz.node(name=str(index), label=str(index))
+        self.__G.add_node(node, **attr)
+        if ("\t" + str(node.value) + " [label=" + str(node.value) + "]\n") not in self.__G_graphviz.body:
+            self.__G_graphviz.node(name=str(node.value), label=str(node.value))
 
-    def add_edge(self, from_node: int, to_node: int, weight: int = 1):
+    def add_edge(self, from_node: Node, to_node: Node, weight: int = 1):
         self.__G.add_edge(from_node, to_node, weight=weight, color="black")
-        self.__add_edge_graphviz(str(from_node), str(to_node), str(weight))
+        self.__add_edge_graphviz(str(from_node.value), str(to_node.value), str(weight))
 
     def __add_edge_graphviz(self, from_node: str, to_node: str, weight: int = 1):
         if ("\t" + from_node + " [label=" + from_node + "]\n") not in self.__G_graphviz.body:
@@ -81,6 +93,8 @@ class Graph:
 
     def visualize(self):
         pos = nx.circular_layout(self.__G)
+        node_labels = {u: u.value for u in self.G.nodes()}
+        print(node_labels)
         options_node = {
             'node_color': 'deeppink',
             'node_size': 400,
@@ -90,8 +104,11 @@ class Graph:
         }
         colors = [self.G[u][v]['color'] for u, v in self.G.edges()]
         edge_width = [3 if (self.G[u][v]["color"] == "deeppink") else 1 for u, v in self.G.edges()]
-        nx.draw(self.__G, pos, with_labels=True, edge_color=colors, width=edge_width, arrows=self.__directed)
+        nx.draw(self.__G, pos, labels=node_labels, with_labels=True, edge_color=colors, width=edge_width,
+                arrows=self.__directed)
         nx.draw_networkx_nodes(self.__G, pos, **options_node)
+        # nx.draw_networkx_labels(self.G, pos, node_labels, font_size=16, font_color='r')
+
         if self.__directed:
             nx.draw_networkx_edge_labels(self.__G, pos,
                                          edge_labels={(u, v): d for u, v, d in self.__G.edges(data="weight")},
@@ -101,21 +118,52 @@ class Graph:
             nx.draw_networkx_edge_labels(self.__G, pos, edge_labels=edge_labels, label_pos=.66)
         plt.show()
 
+    """
+        BFS -- breadth first search
+    """
+
+    def BFS_basic(self, start: int):
+        nx.bfs_tree()
+        queue = Queue()
+        queue.put(start)
+        while not queue.empty():
+            pass
+
+    def BFS_attributes(self, start: int):
+        pass
+
 
 graph = Graph(True)
-graph.add_edge(0, 1, 10)
-graph.add_edge(1, 2, 15)
-graph.add_edge(1, 3)
-graph.add_edge(2, 5)
-graph.add_edge(2, 6)
-graph.add_edge(3, 4)
-graph.add_edge(4, 5)
-graph.add_edge(5, 2)
-graph.add_edge(6, 1)
-graph.add_edge(6, 4)
+node_00 = Node("Zlín")
+node_01 = Node("Praha")
+node_02 = Node("Brno")
+node_03 = Node(3)
+node_04 = Node(4)
+node_05 = Node(5)
+node_06 = Node(6)
+
+graph.add_node(node_01)
+graph.add_node(node_02)
+graph.add_node(node_03)
+graph.add_node(node_04)
+graph.add_node(node_05)
+graph.add_node(node_06)
+
+graph.add_edge(node_00, node_01, 10)
+graph.add_edge(node_01, node_02, 15)
+graph.add_edge(node_01, node_03)
+graph.add_edge(node_02, node_05)
+graph.add_edge(node_02, node_06)
+graph.add_edge(node_03, node_04)
+graph.add_edge(node_04, node_05)
+graph.add_edge(node_05, node_02)
+graph.add_edge(node_06, node_01)
+graph.add_edge(node_06, node_04)
 graph.print_adj_matrix()
 graph.visualize()
-
+print(graph.get_neighbours(1))
+print(graph.get_neighbours(6))
+print(graph.get_neighbours(0))
 # graph = Graph(False)
 # graph.add_node(1)
 # graph.add_node(2)
